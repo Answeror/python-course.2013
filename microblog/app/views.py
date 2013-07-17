719c0a92b1ffaeedd90c0fdc9ae87d67685729f9
answeror <answeror@gmail.com>
2013-07-18 01:11:20 +0800

    done

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -3,4 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world!'
+    user = {'nickname': 'Miguel'}
+    return '''
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['nickname'] + '''</h1>
+    </body>
+</html>
+'''
