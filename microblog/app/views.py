b8cb2f9add083cd93ec065a70e976660725317fd
answeror <answeror@gmail.com>
2013-07-18 01:28:49 +0800

    done

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -5,8 +5,19 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     return render_template(
         'index.html',
         title='Home',
-        user=user
+        user=user,
+        posts=posts
     )
