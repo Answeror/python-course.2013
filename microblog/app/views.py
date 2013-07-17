a3f3dd14e02f02d259e8bc3002deec25fbec6767
answeror <answeror@gmail.com>
2013-07-18 01:15:05 +0800

    done

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,6 +1,12 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world!'
+    user = {'nickname': 'Miguel'}
+    return render_template(
+        'index.html',
+        title='Home',
+        user=user
+    )
